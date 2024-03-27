eee05af99d788c6a3531205332243623b4d37669
t4i5e1 <astraea.nymph.08@gmail.com>
2024-03-27 23:24:04 +0900

    first commit

---
+++ example.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //変数の宣言
+            int money;  //所持金を代入する変数
+            string name;    //名前を代入する変数
+
+            //変数を値を代入する
+            money = 5000;
+            name = "さくらみこ";
+
+            Console.WriteLine(money);   //所持金を表示
+            Console.WriteLine(name);    //名前を表示
+        }
+    }
+}
