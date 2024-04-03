0f37b444e32b936710d1819921ebc7ba48f1cd4f
t4i5e1 <astraea.nymph.08@gmail.com>
2024-04-03 23:33:57 +0900

    caluculateTest

---
+++ caluculateTest.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer;
+            answer = 1 + 2 + 3 + 4 + 5;
+            Console.WriteLine(answer);
+
+            answer = 30 % 7;
+            Console.WriteLine(answer);
+        }
+    }
+}
