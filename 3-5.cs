0f9639bff69fa903b7bcce0d5c429593ba24f92d
t4i5e1 <astraea.nymph.08@gmail.com>
2024-04-04 22:57:20 +0900

    floattest

--- 3-5.cs
+++ 3-5.cs
@@ -3,4 +3,17 @@ using System;
 namespace Example
 {
     class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 480
+            int t = 513
+            int w = 500
+            int t2 = 480
+            int F = 490
+
+            float average = (m + t + w + t2 + f) / 5.0f
+            console.writeline(average;)
+        }
+    }
 }
