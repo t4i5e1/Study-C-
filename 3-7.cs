24270b495ddf1a7e1ded992e9b869a14f82276b8
t4i5e1 <astraea.nymph.08@gmail.com>
2024-04-05 00:00:42 +0900

    3-7

---
+++ 3-7.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+
+            if (num >= 3)
+            {
+                Console.WriteLine("勝ち");
+            }
+        }
+    }
+}
