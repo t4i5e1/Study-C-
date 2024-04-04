c38eff7e38739cef4cd629168eccb3328e5a63eb
t4i5e1 <astraea.nymph.08@gmail.com>
2024-04-04 23:11:26 +0900

    add3-6

---
+++ 3-6.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            a = --;
+            Console.WriteLine(a);
+        }
+    }
+}
