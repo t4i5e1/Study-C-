d8b494105bfae1af6147e8140db27d399ff41ad3
t4i5e1 <astraea.nymph.08@gmail.com>
2024-04-09 00:00:34 +0900

    3-9test

---
+++ 3-9test.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int point = 2;
+
+            if (point >= 3)
+            {
+                Console.WriteLine("勝ち");
+            }
+            else
+            {
+                Console.WriteLine("負け");
+            }
+        }
+    }
+}
