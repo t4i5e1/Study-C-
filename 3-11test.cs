84a26b515452881d498af9d657f654aef1a0686a
t4i5e1 <astraea.nymph.08@gmail.com>
2024-04-10 23:44:33 +0900

    3-11test

---
+++ 3-11test.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("逃走!");
+            }
+        }
+    }
+}
