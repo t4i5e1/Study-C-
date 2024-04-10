168cc630c20e83953f0ba8675421a285684778c5
t4i5e1 <astraea.nymph.08@gmail.com>
2024-04-11 00:03:01 +0900

    add3-12b

---
+++ 3-12b.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
